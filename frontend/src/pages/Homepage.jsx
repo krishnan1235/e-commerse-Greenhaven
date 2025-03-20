e75d729b84d4f6c9479e587cf63783a234733429
Hariprasath2030 <webhari2005@gmail.com>
2025-03-20 18:37:49 +0530

    push

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,5 +1,4 @@
-import { Container, SimpleGrid, Text, VStack } from '@chakra-ui/react';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import ProductCard from './productcard';
