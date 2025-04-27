31abc3306c3ec539bcd8d35a284c86c1f6089357
krishnan1235 <133309868+krishnan1235@users.noreply.github.com>
2025-04-27 15:18:04 +0530

    changes for deploy

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://greenhaven-liart.vercel.app",
 }));
 
 app.use(express.json()); // Allows us to accept JSON data in req.body
